c851d885157bfd07b33dd281b4e531879b47d316
redricasa <fredeb26@gmail.com>
2019-09-29 21:04:58 -0700

    added switch cases for all the 4 CLI commands, as well as a function to take the index of 2 and 3 using process.argv, got the request with a promise code from npm website for node-spotify-api, and npm installed node-spotify-api as well

--- LIRI.js
+++ LIRI.js
@@ -1,3 +1,41 @@
 require("dotenv").config();
+//require files and npm modules here first
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+//gets id and secret from keys.js files
 var spotify = new Spotify(keys.spotify);
+//spotify API request with a promise
+spotify
+.search({ type: 'track', query: 'All the Small Things', limit:5 })
+.then(function(response) {
+    console.log(response);
+})
+.catch(function(err) {
+    console.log(err);
+});
+
+//switch cases to hold the different commands from user
+var choose = function (caseData, functionData){
+    switch(caseData) {
+        case 'do-what-it-says':
+            //insert call back 
+            break;
+        case 'spotify-this-song':
+            //insert call back
+            break;
+        case 'movie-this':
+            //insert call back
+            break;
+        case 'concert-this':
+            //insert call back
+            break;
+        default:
+        console.log('LIRI does not do that');
+    }
+}
+//function to pass arguments into switch case 
+var clientToServer = function(command,item){
+    choose(command, item)
+}
+//call back function will take in the command as index[2] and the item searching for as index[3]
+clientToServer(process.argv[2], process.argv[3]);
